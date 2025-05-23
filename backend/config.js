0b8d5447f29f1a4b3238091ffdb88c7849b590ca
sandr3120 <sandramanoj.sm60@gmail.com>
2025-05-23 11:56:28 +0530

    Added MongoDB to Project

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const uri = "mongodb+srv://root:q17pnxpO4FFIEG2L@book-store-mern.yyebhqh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN";
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, uri } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.get('/', (request,response)=>{
-    console.log(request);
-    return response.status(234).send(`Welcome to Book Store`);
-})
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log(`App connected to database`);
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-app.listen(PORT, ()=>{
-    console.log(`App is listening to port: ${PORT}`);
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send(`Welcome to Book Store`);
 });
-
