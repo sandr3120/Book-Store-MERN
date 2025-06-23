acf8b2deaa25dbc58db943e6ede31d048fe51c67
sandr3120 <sandramanoj.sm60@gmail.com>
2025-06-23 19:52:03 +0530

    Books List Page Completed

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,12 @@ import cors from "cors";
 
 const app = express();
 
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/books", booksRouter);
+
 mongoose
   .connect(uri)
   .then(() => {
@@ -24,14 +30,3 @@ app.get("/", (request, response) => {
   return response.status(234).send(`Welcome to Book Store`);
 });
 
-app.use(express.json());
-
-app.use("/books", booksRouter);
-
-app.use(
-  cors({
-    origin: "https://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="animate-ping w-16 h-16 m-8 rounded-full bg-slate-200"></div>
+    </div>
+  )
+}
+
+export default Spinner
